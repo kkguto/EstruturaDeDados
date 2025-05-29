10b494b4e1ffaff89d14a53634a0fad02c0d968f
Guto <guto050506@gmail.com>
2025-05-29 15:07:01 -0300

    inicio

---
+++ EstruturaDeDados/src/estrutura/ListaLigadaLinear/ListaSimples/Simples.java
@@ -0,0 +1,5 @@
+package estrutura.ListaLigadaLinear.ListaSimples;
+
+public class Simples {
+
+}
---
+++ EstruturaDeDados/src/run/Main.java
@@ -0,0 +1,6 @@
+package run;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
